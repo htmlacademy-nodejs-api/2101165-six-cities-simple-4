7ccfd2c9dcb7b98ecee15b3008c85a385ab04e19
Dmitry Bochenkov <pishisudapridurok@yandex.ru>
2023-06-24 13:03:14 +0300

    123

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -20,6 +20,7 @@ export default class RestApplication {
     @inject(AppComponent.CityController) private readonly cityController: ControllerInterface,
     @inject(AppComponent.ExceptionFilterInterface) private readonly exceptionFilter: ExceptionFilterInterface,
     @inject(AppComponent.UserController) private readonly userController: ControllerInterface,
+    @inject(AppComponent.OfferController) private readonly offerController: ControllerInterface,
   ) {
     this.expressApplication = express();
   }
@@ -52,6 +53,7 @@ export default class RestApplication {
     this.logger.info('Controller initialization…');
     this.expressApplication.use('/cities', this.cityController.router);
     this.expressApplication.use('/users', this.userController.router);
+    this.expressApplication.use('/offers', this.offerController.router);
     this.logger.info('Controller initialization completed');
   }
 
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import { createRestApplicationContainer } from './app/rest.container.js';
 import { createUserContainer } from './modules/user/user.container.js';
 import { createOfferContainer } from './modules/offer/offer.container.js';
 import { createCommentContainer } from './modules/comment/comment.container.js';
+import { createCityContainer } from './modules/city/city.container.js';
 
 async function bootstrap() {
   const mainContainer = Container.merge(
     createRestApplicationContainer(),
+    createCityContainer(),
     createUserContainer(),
     createOfferContainer(),
     createCommentContainer(),
